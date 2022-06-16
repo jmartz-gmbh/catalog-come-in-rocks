25f0831c7b3eaa48078c5a92c7a3a3147fbb4422
Jonathan Martz <git@jmartz.de>
2022-06-16 10:00:14 +0200

    add config folder

---
+++ config/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'ccrcatalog'),
+      user: env('DATABASE_USERNAME', 'ccrcatalog'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    debug: false,
+  },
+});
+
